51d2609bbedb9c3fc53e58b3043644fe4d245f00
BuildTools <unconfigured@null.spigotmc.org>
2021-01-20 11:47:07 +1100

    Pipe circumferences mostly donzo

--- scripts/script.js
+++ scripts/script.js
@@ -7,11 +7,18 @@ window.addEventListener("load", () => {
 
 
 // Pipe circumference Calculations
+//WRAP THIS INTO AN ONLOAD FOR THE PIPE FILE AS TO NOT GENERATE ERRORS
 
-var pipeSize = 300;
-var circumference;
-var ibs;
-var unicollar;
+function pipeChart() {
+  
+}
+var pipe = document.getElementById("pipeSize");
+var circumference = document.getElementById("circumference");
+var ibs = document.getElementById("ibs")
+var unicollar = document.getElementById("unicollar");
 
-circumference = circumference * Math.PI
-
+pipe.addEventListener('input', function() {
+  circumference.innerHTML = parseFloat(pipe.innerHTML * Math.PI).toFixed(2);
+  ibs.innerHTML = parseFloat(Math.ceil((circumference.innerHTML / 1000) * 100) / 100).toFixed(2);
+  unicollar.innerHTML = parseFloat(Math.ceil((circumference.innerHTML / 2250) * 100) / 100).toFixed(2);
+});
