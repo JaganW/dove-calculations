8a9d93f8dcf7b1f67dd66a10b088658e3f16ae00
JaganW <jaganwoods0@gmail.com>
2021-01-21 08:38:38 +1100

    Pipe Circumferences Finished :)))

--- scripts/script.js
+++ scripts/script.js
@@ -12,13 +12,25 @@ window.addEventListener("load", () => {
 function pipeChart() {
   
 }
+
 var pipe = document.getElementById("pipeSize");
 var circumference = document.getElementById("circumference");
 var ibs = document.getElementById("ibs")
 var unicollar = document.getElementById("unicollar");
 
 pipe.addEventListener('input', function() {
-  circumference.innerHTML = parseFloat(pipe.innerHTML * Math.PI).toFixed(2);
-  ibs.innerHTML = parseFloat(Math.ceil((circumference.innerHTML / 1000) * 100) / 100).toFixed(2);
-  unicollar.innerHTML = parseFloat(Math.ceil((circumference.innerHTML / 2250) * 100) / 100).toFixed(2);
+  if(isNaN(pipe.innerHTML)) {
+    showAlert();
+  } else {
+    circumference.innerHTML = parseFloat(pipe.innerHTML * Math.PI).toFixed(2);
+    ibs.innerHTML = parseFloat(Math.ceil((circumference.innerHTML / 1000) * 100) / 100).toFixed(2);
+    unicollar.innerHTML = parseFloat(Math.ceil((circumference.innerHTML / 2250) * 100) / 100).toFixed(2);  
+  }
 });
+
+
+function showAlert() {
+  if(!document.getElementById('pipeAlert')) {
+    document.getElementById('pipeAlertPlace').innerHTML += '<div class="alert alert-warning alert-dismissible p-2 fade show" role="alert" id="pipeAlert"><strong>Note:</strong> Please use only numbers in the cell above.<button type="btn" class="btn-close pb-1" data-bs-dismiss="alert" aria-label="close"></button></div>';
+  }
+}
