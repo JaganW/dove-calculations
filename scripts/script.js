a9d8ac0749768a130acbe0dea2dd555d4aef483a
JaganW <jaganwoods0@gmail.com>
2021-01-20 16:03:30 +1100

    end of day @ dove

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,15 @@ window.addEventListener("load", () => {
     navigator.serviceWorker.register("/app/scripts/service-worker.js", { scope: '/app/'});
   }
 });
+
+
+
+// Pipe circumference Calculations
+
+var pipeSize = 300;
+var circumference;
+var ibs;
+var unicollar;
+
+circumference = circumference * Math.PI
+
